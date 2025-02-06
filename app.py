95e89009632b75b58e05c52678de7097e65ff27d
onurcangnc <onurcan.genc@ug.bilkent.edu.tr>
2025-02-06 11:00:20 +0300

    done

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ es = Elasticsearch(
     basic_auth=(USERNAME, PASSWORD),
     request_timeout=120,
     max_retries=10,
-    retry_on_timeout=True
+    retry_on_timeout=True,
     verify_certs=True,  # SSL sertifikasını doğrula
     ca_certs="C:/Users/Administrator/Desktop/leak/elasticsearch-8.17.1/config/ca.crt"
 )
